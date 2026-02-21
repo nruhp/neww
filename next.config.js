4ed09bfcbfb869c2fffa4811fdafc4c1ce8420a7
nruhp <nruhp25@gmail.com>
2026-02-21 22:58:27 +0530

    Frontend separated from backend

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost', 'newskprinter-9z4p.vercel.app'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+ images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: '**.vercel.app',
+    },
+  ],
+},
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
   },
